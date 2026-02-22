640695d0172c2e9adf23cb10aca8c2f714eee9e3
yeftakun <yeftakun34@gmail.com>
2026-02-22 13:01:51 +0800

    .

--- update-from-template.bat
+++ update-from-template.bat
@@ -1,19 +1,24 @@
 @echo off
 setlocal
 
-rem Ensure .gitattributes exists with merge rule for personal data
-echo [1/8] Preparing .gitattributes for locally-kept files...
+echo [1/8] Ensuring .gitattributes merge rules...
 if not exist .gitattributes (
-  rem keep local preferences from being overwritten
-  echo data/personalbest.ini merge=ours>.gitattributes
-  echo src/data/config.json merge=ours>>.gitattributes
-  echo src/data/laptime.json merge=ours>>.gitattributes
-  echo src/data/temp_laptime.json merge=ours>>.gitattributes
-  echo src/data/old_laptime.json merge=ours>>.gitattributes
-  echo src/data/meta.json merge=ours>>.gitattributes
-  echo update-from-template.bat merge=ours>>.gitattributes
-  echo README.md merge=ours>>.gitattributes
+  type nul > .gitattributes
 )
+findstr /C:"src/data/meta.json merge=ours" .gitattributes >nul || ^
+  echo src/data/meta.json merge=ours>>.gitattributes
+findstr /C:"src/data/laptime.json merge=ours" .gitattributes >nul || ^
+  echo src/data/laptime.json merge=ours>>.gitattributes
+findstr /C:"src/data/temp_laptime.json merge=ours" .gitattributes >nul || ^
+  echo src/data/temp_laptime.json merge=ours>>.gitattributes
+findstr /C:"src/data/old_laptime.json merge=ours" .gitattributes >nul || ^
+  echo src/data/old_laptime.json merge=ours>>.gitattributes
+findstr /C:"data/personalbest.ini merge=ours" .gitattributes >nul || ^
+  echo data/personalbest.ini merge=ours>>.gitattributes
+findstr /C:"update-from-template.bat merge=ours" .gitattributes >nul || ^
+  echo update-from-template.bat merge=ours>>.gitattributes
+findstr /C:"README.md merge=ours" .gitattributes >nul || ^
+  echo README.md merge=ours>>.gitattributes
 
 echo [2/8] Checking for remote "upstream"...
 for /f "tokens=*" %%r in ('git remote') do (
@@ -21,24 +26,22 @@ for /f "tokens=*" %%r in ('git remote') do (
 )
 if not defined HAS_UPSTREAM (
   echo        Adding remote upstream...
-  git remote add upstream https://github.com/yeftakun/ac-lapboard.git
+  git remote add upstream https://github.com/yeftakun/ac-lapboard.git || goto :error
 )
 
 echo [3/8] Waiting for confirmation before update...
 set /p _="Need to update? (Enter) "
 
 echo [4/8] Fetching changes from upstream and origin...
-git fetch upstream
-if errorlevel 1 goto :error
-git fetch origin
-if errorlevel 1 goto :error
+git fetch upstream || goto :error
+git fetch origin || goto :error
 
 echo [5/8] Merging changes from upstream/master...
 git merge upstream/master -m "update from template"
 if errorlevel 1 goto :error
 
 echo [6/8] Rebase onto origin/master to include remote changes...
-git pull --rebase origin master
+git pull --rebase --autostash origin master
 if errorlevel 1 goto :error
 
 echo [7/8] Showing status...
